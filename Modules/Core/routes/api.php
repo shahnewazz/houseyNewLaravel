b5e0f946d5dffeb58071f43a09fe84ea4d5532f6
shahnewazz <shahnewaz720@gmail.com>
2024-09-24 22:39:49 +0600

    added

--- Modules/Core/routes/api.php
+++ Modules/Core/routes/api.php
@@ -13,7 +13,3 @@ use Modules\Core\Http\Controllers\CoreController;
  * is assigned the "api" middleware group. Enjoy building your API!
  *
 */
-
-Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::apiResource('core', CoreController::class)->names('core');
-});
